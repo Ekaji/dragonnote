f7e487377992378b7b65e8940d680be78d72dbe0
ekaji <ekajionah@gmail.com>
2021-05-31 22:18:17 +0100

    used context api to trigger rerender of flat list to toggle checkboxes

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 // force the state to clear with fast refresh in Expo
 // @refresh reset
 import React from 'react';
-import { useState, useEffect, useLayoutEffect} from 'react';
+import { useState, useEffect, useLayoutEffect, useContext} from 'react';
 import { ScrollView, View, Text, FlatList, TouchableOpacity, Button, StyleSheet} from 'react-native'
 import styled from 'styled-components/native';
 import CheckBox from '@react-native-community/checkbox';
@@ -9,48 +9,33 @@ import CreateNoteButton from './CreateNoteButton';
 import { useNavigation } from '@react-navigation/native';
 import { LongPressGestureHandler, State } from 'react-native-gesture-handler';
 
+import { CheckboxContext } from './context/CheckBoxContext'
+
 import { database } from './components/Database';
 
 //discribes how the rendered item should look
-const NotesInfo = ({ id, title, content }) => {
+const NotesInfo = ({ id, title, content, loadDataAsync}) => {
     const navigation = useNavigation();
+    //checkbox toggle
     const [toggleCheckBox, setToggleCheckBox] = useState(false)
-    const [showheckboixes, setShowCheckBoxes] = useState(false)
+    //applies css styling to either show or hide checkbox
+    const [checkBoxDisplay, setCheckBoxDisplay] = useContext(CheckboxContext)
 
-    const [currentCheckBox, setCurrentCheckBox] = useState('')
-    const [checkBoxIds, setCheckBoxIds] = useState([])
-    
-    const Notesview = styled.View`
-        margin: 20px;
-    `;
-    const Text_Title = styled.Text`
-        font-size: 20px;
-        margin-bottom: 8px;
-    `;
-    
-        const onLongPress = (event) => {
-            if (event.nativeEvent.state === State.ACTIVE) {
-              console.log("I've been pressed for 800 milliseconds");
-              setShowCheckBoxes(!showheckboixes)
-            }
-          };
+    const [checkBoxIDs, getCheckBoxIDs] = useState([])
 
-        const getChecked = (ID) => {
-            setCurrentCheckBox(ID);
-            setCheckBoxIds([...checkBoxIds, currentCheckBox])
+    const onLongPress = (event) => {
+        if (event.nativeEvent.state === State.ACTIVE) {
+        setCheckBoxDisplay(checkBoxDisplay === styles.hideBox ? styles.showBox : styles.hideBox)
+        console.log("I've been pressed for 800 milliseconds");
         }
-
-        // useEffect(() => {
-        //     setCheckBoxIds([...checkBoxIds, currentCheckBox])
-        //     console.log('checkBoxIds',checkBoxIds)
-        // },)
+    };
 
 
     return(
         <TouchableOpacity  onPress={() => navigation.navigate('Createnote', 
             {id: id, title: title, content: content}
         )}>
-             <LongPressGestureHandler
+            <LongPressGestureHandler
             onHandlerStateChange={onLongPress}
             minDurationMs={800}
         >
@@ -59,20 +44,25 @@ const NotesInfo = ({ id, title, content }) => {
                 <Text_Title>{ title }</Text_Title>
                 <Text>{ content }</Text>
             </Notesview>
-            {showheckboixes === !true? <CheckBox
+
+            <CheckBox
+                style={checkBoxDisplay}
                 disabled={false}
                 value={toggleCheckBox}
-                onValueChange={() => { 
-                    getChecked(id)
-                    setToggleCheckBox(!toggleCheckBox)
-                    toggleCheckBox === !true? database.deleteNote(id) : null    
+                tintColors = {{ true: '#FF6900' , false: '#d4d4d4' }}
+                onChange={() => {
+                    setToggleCheckBox(!toggleCheckBox);
+                    getCheckBoxIDs([...checkBoxIDs, id])
+                    console.log('IDS',checkBoxIDs)
+                    // togggleCheckboxFunc()
+                    // toggleCheckBox === !true ? database.deleteNote(id) : null
+                    //reload after delete
+                    loadDataAsync();    
                 }
             }
-            /> : null
-        } 
+            />
             </View>
             </LongPressGestureHandler>
-        
         </TouchableOpacity>
     )  
 }
@@ -80,6 +70,8 @@ const NotesInfo = ({ id, title, content }) => {
 
 const Home = ({navigation}) => {
     const [ note, getNote ] = useState([]);
+    //passed as value to context
+    const [checkBoxDisplay, setCheckBoxDisplay] = useState(styles.hideBox)
     console.log(note._array, 'testing' )
 
 const loadDataAsync = async () => {
@@ -95,26 +87,27 @@ const loadDataAsync = async () => {
     }
   }
 
+    useEffect(() => {
+        //fetch data when the home screen comes into focus
+        const reloadData = navigation.addListener('focus', () => {
+            loadDataAsync();
+            } )
+            return reloadData
+    })
 
     useEffect(() => {
         //loads data when app starts the first time
         loadDataAsync();
-        //fetch data when the home screen comes into focus
-        const reloadData = navigation.addListener('focus', () => {
-            loadDataAsync();
-        })
-        return reloadData
    },[])
-
 
     const renderItem = ( {item} ) => {
         return(
-            <NotesInfo title={item.title} content={item.content} id={item.id} />
+            <NotesInfo title={item.title} content={item.content} id={item.id} loadDataAsync={loadDataAsync} />
         )
     };
 
     return(
-         <>
+         <CheckboxContext.Provider value={[checkBoxDisplay, setCheckBoxDisplay]}>
             <FlatList  
                 style= {{ display: 'flex', flexDirection: 'column-reverse'}}
                 data={note._array}
@@ -123,8 +116,30 @@ const loadDataAsync = async () => {
                 inverted={true}
             />
           <CreateNoteButton /> 
-         </>
+         </CheckboxContext.Provider>
     )
 };
+
+const Notesview = styled.View`
+margin: 20px;
+width: 70%;
+`;
+const Text_Title = styled.Text`
+font-size: 20px;
+margin-bottom: 8px;
+`;
+
+const styles = StyleSheet.create({
+hideBox : {
+    opacity: 0,
+    height: 0,
+},
+showBox : {
+    marginTop: 50,
+    marginRight: 12,
+    opacity: 1,
+    height: 9,
+}
+})
 
 export default Home;
